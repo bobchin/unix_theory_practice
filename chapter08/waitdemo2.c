b624bd4b93383934514caf2bc4632836a2e8d811
bobchin <bobchin.ryu@gmail.com>
2015-10-01 11:42:15 +0900

    waitdemo2

---
+++ chapter08/waitdemo2.c
@@ -0,0 +1,56 @@
+/*
+ * waitdemo2.c - 親は子のステータスをどのように取得さればよいかを示す
+ */
+#include <stdio.h>
+
+#include <unistd.h>
+#include <stdlib.h>
+
+#define DELAY 5
+
+int main(void)
+{
+  int newpid;
+  void child_code(), parent_code();
+
+  printf("before: my pid is %d\n", getpid());
+
+  if ((newpid = fork()) == -1) {
+    perror("fork");
+  } else if (newpid == 0) {
+    child_code(DELAY);
+  } else {
+    parent_code(newpid);
+  }
+
+  return 0;
+}
+
+/*
+ * 新プロセスは昼寝して終了する
+ */
+void child_code(int delay)
+{
+  printf("child %d here. will sleep for %d seconds\n", getpid(), delay);
+  sleep(delay);
+  printf("child done. about to exit.\n");
+  exit(17);
+}
+
+/*
+ * 親は子が終了するのを待ち、メッセージを出力する
+ */
+void parent_code(int childpid)
+{
+  int wait_rv;
+  int child_status;
+  int high_8, low_7, bit_7;
+
+  wait_rv = wait(&child_status);
+  printf("done waiting for %d. Wait returned: %d\n", childpid, wait_rv);
+
+  high_8 = child_status >> 8;   /* 1111 1111 0000 0000 */
+  low_7  = child_status & 0x7F; /* 0000 0000 0111 1111 */
+  bit_7  = child_status & 0x80; /* 0000 0000 1000 0000 */
+  printf("status: exit=%d, sig=%d, core=%d\n", high_8, low_7, bit_7);
+}
